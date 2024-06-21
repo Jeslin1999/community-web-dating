8575a6c17f5d7ca59b5f384903f63e807d875322
Jeslin1999 <jeslinelizabeth8606@gmail.com>
2024-06-21 13:55:32 +0530

    registration sucess

--- BASE/Dating/views.py
+++ BASE/Dating/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse,redirect
 from django.contrib.auth import authenticate,login
 from .forms import LoginForm,RegisterFirstFrom
+from .models import User
 
 
 def user_login(request):
@@ -26,34 +27,43 @@ def user_login(request):
         return render(request,'Dating/login.html',context)
     
 
+def register(request):
+    context = {}
+    if request.method == 'GET':
+        context['form'] = RegisterFirstFrom()
+        return render(request,'Dating/register.html',context)
+    elif request.method == 'POST':
+        form = RegisterFirstFrom(request.POST)
+        if form.is_valid():        
+            user = form.cleaned_data
+            data = User(
+                first_name = user['first_name'],
+                last_name = user['last_name'],
+                email = user['email'],
+                username = user['username'],
+                password = user['password'],
+                dob = user['dob'],
+                age = user['age']
+                )
+            data.save()
+            return redirect('../login')
+        else:
+            form = RegisterFirstFrom()
+    
+        context['form'] = RegisterFirstFrom()
+        return render(request,'Dating/register.html',context)
+
+
 # def register(request):
-#     context = {}
-#     if request.method == 'GET':
-#         context['form'] = RegisterFirstFrom()
-#         return render(request,'Dating/register.html',context)
-#     elif request.method == 'POST':
-#         form = RegisterFirstFrom(request.POST)
-#         if form.is_valid():        
-#             user = form.save(commit=False)
-#             user.set_password(user.cleaned_data['password'])
+#     if request.method == 'POST':
+#         form1 = RegisterFirstFrom(request.POST)
+#         if form1.is_valid():
+#             user = form1.save(commit = False)
+#             user.set_password(form1.cleaned_data['password'])
 #             user.save()
 #             return redirect('login')
-#         else:
-#             # return render(request,'Dating/register.html')
-#             context['form'] = RegisterFirstFrom()
-#             return render(request,'Dating/register.html',context)
-
-
-def register(request):
-    if request.method == 'POST':
-        form = RegisterFirstFrom(request.POST)
-        if form.is_valid():
-            user = form.save(commit = False)
-            user.set_password(form.cleaned_data['password'])
-            user.save()
-            return redirect('login')
-    else:
-        form = RegisterFirstFrom()
+#     else:
+#         form1 = RegisterFirstFrom()
     
-    return render(request, 'Dating/register.html', {'form': form})
+#     return render(request, 'Dating/register.html', {'form': form1})
     
